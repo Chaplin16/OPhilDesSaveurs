295f7b82108e710605714967549460af45cf9d00
Chaplin16 <chaplin16@orange.fr>
2021-10-08 17:11:33 +0200

    mise en place du carrousel et ajout texte

---
+++ js/dish.js
@@ -0,0 +1,48 @@
+const dish = [
+    {
+        id:1,
+        attachment: "images/dish/crepes-jambon-marche-aigues-mortes.jpg",
+        alt:"crepess faites maison au jambon cuisinees a aigues mortes par philippe"
+    },
+    {
+        id:2,
+        attachment: "images/dish/cuisine-sur-marche-aigues-mortes.jpg",
+        alt:"daube faite maison cuisinee par philippe au marche d aigues mortes"
+    },
+    {
+        id:3,
+        attachment: "images/dish/gratin-cuisine-marche-aigues-mortes.jpg",
+        alt:"plats fait maison cuisine par o phil des saveurs "
+    },
+    {
+        id:4,
+        attachment: "images/dish/gratin-cuisine-sur-marche-aigues-mortes.jpg",
+        alt:"gratin fait maison vendu au marche de saint laurent d aigouze par o phil des saveurs"
+    },
+    {
+        id:5,
+        attachment: "images/dish/lasagne-cuisine-marche-aigues-mortes.jpg",
+        alt:"plats de lasagne faites maison cuisines au marche d aigues mortes"
+    },
+    {
+        id:6,
+        attachment: "images/dish/moules-cuisinees-marche-aigues-mortes.jpg",
+        alt:"plats faits maison cuisines au marche par philippe a aigues mortes"
+    },
+
+    {
+        id:7,
+        attachment: "images/dish/rouille-cuisine-marche-aigues-mortes.jpg",
+        alt:"plats cuisines de rouille au marche de saint laurent d aigouze"
+    },
+    {
+        id:8,
+        attachment: "images/dish/taboule-cuisine-marche-aigues-mortes.jpg",
+        alt:"taboule o phil des saveurs au marche d aigues mortes"
+    },
+    {
+        id:9,
+        attachment: "images/dish/plat-cuisiné-sur-marche-aigues-mortes.jpg",
+        alt:"gratin en plat cuisine au marche par philippe o phil des saveurs "
+    }
+]
