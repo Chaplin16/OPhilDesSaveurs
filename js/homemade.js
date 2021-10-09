8acb422360014e0af8b7b8ae2402dd91dc3fa803
Chaplin16 <chaplin16@orange.fr>
2021-10-09 22:58:17 +0200

    resize img homemade

---
+++ js/homemade.js
@@ -0,0 +1,72 @@
+const homemade = [
+    {
+        id: 1,
+        attachment: "images/homemade/fait-maison-crepe-o-phil-des-saveurs.jpg",
+        alt:""
+    },
+    {
+        id: 2,
+        attachment: "images/homemade/fait-maiosn-viande-origine-france.jpg",
+        alt:""
+    },
+    {
+        id: 3,
+        attachment: "images/homemade/fais-maison-encornet-philippe-marche.jpg",
+        alt:""
+    },
+    {
+        id: 4,
+        attachment: "images/homemade/fresh-pasta-lasagne.jpg",
+        alt:""
+    },
+    {
+        id: 5,
+        attachment: "images/homemade/fait-maison-viande-francaise.jpg",
+        alt:""
+    },
+    {
+        id: 6,
+        attachment: "images/homemade/fait-maison-vegetables.jpg,"
+        alt:""
+    },
+    {
+        id: 7,
+        attachment: "images/homemade/fait-maison-saveur-o-phil-des-saveurs.jpg",
+        alt:""
+    },
+    {
+        id: 8,
+        attachment: "images/homemade/fait-maison-poisson-frais-viande-francaise.jpg",
+        alt:""
+    },
+    {
+        id: 9,
+        attachment: "images/homemade/fait-maison-pate-crepe-o-phil-des-saveurs.jpg",
+        alt:""
+    },
+    {
+        id: 10,
+        attachment: "images/homemade/fait-maison-o-phil-des-saveurs.jpg",
+        alt:""
+    },
+    {
+        id: 11,
+        attachment: "images/homemade/fait-maison-oignon-frais.jpg",
+        alt:""
+    },
+    {
+        id: 12,
+        attachment: "images/homemade/fait-maison-legumes-frais-marche-aigues-mortes.jpg",
+        alt:""
+    },
+    {
+        id: 13,
+        attachment: "images/homemade/fait-maison-legumes-de-saison.jpg",
+        alt:""
+    },
+    {
+        id: 14,
+        attachment: "images/homemade/fait-maison-legume-frais.jpg",
+        alt:""
+    }
+]
