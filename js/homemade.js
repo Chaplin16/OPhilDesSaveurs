7b48be2c269c237ac4a4e0a78b34bb547d3f3e1d
Chaplin16 <chaplin16@orange.fr>
2021-10-10 17:56:04 +0200

    refacto images

--- js/homemade.js
+++ js/homemade.js
@@ -2,72 +2,72 @@ const homemade = [
     {
         id: 4,
         attachment: "images/homemade/fresh-pasta-lasagne.jpg",
-        alt:""
+        alt:"pate faiche 100% fait maison chea o phil des saveurs"
     },
     {
         id: 9,
         attachment: "images/homemade/fait-maison-pate-crepe-o-phil-des-saveurs.jpg",
-        alt:""
+        alt:"crêpes faits-maison aux bon oeufs chez le traiteur Philippe d' o phil des saveurs"
     },
     {
         id: 1,
         attachment: "images/homemade/fait-maison-crepe-o-phil-des-saveurs.jpg",
-        alt:""
+        alt:"crêpes faites-maison au marche d aigues mortes chez o phil des saveurs "
     },
     {
         id: 5,
         attachment: "images/homemade/fait-maison-viande-francaise.jpg",
-        alt:""
+        alt:"viande 100% française au marche d o phil des saveurs"
     },
     {
         id: 2,
         attachment: "images/homemade/fait-maison-viande-origine-france.jpg",
-        alt:""
+        alt:"viande 100% française  par philippe au marche d aigues mortes"
     },
     {
         id: 8,
         attachment: "images/homemade/fait-maison-poisson-frais-viande-francaise.jpg",
-        alt:""
+        alt:"delicieuse rouille fiate 100 pour cent maison par o phil des saveurs"
     },
     {
         id: 3,
         attachment: "images/homemade/fais-maison-encornet-philippe-marche.jpg",
-        alt:""
+        alt:"encornets faits maison a deguster au marche d aigues mortes"
     },
 
     {
         id: 14,
         attachment: "images/homemade/fait-maison-legume-frais.jpg",
-        alt:""
+        alt:"des legumes laves, epluches pour des plats faits maison au marche d aigues mortes"
     },
     {
         id: 7,
         attachment: "images/homemade/fait-maison-saveur-o-phil-des-saveurs.jpg",
-        alt:""
+        alt:"plats cuisines faits maison au marche d aigues mortes par o phil des saveurs"
     },
     {
         id: 6,
         attachment: "images/homemade/fait-maison-vegetables.jpg",
-        alt:""
+        alt:"plats prepares par philippe pour le marche d aigues mortes entierement faits-maison"
     },
     {
         id: 12,
         attachment: "images/homemade/fait-maison-legumes-frais-marche-aigues-mortes.jpg",
-        alt:""
+        alt:"legumes coupes, haches pour des plats prepares faits-maison au marche d aigues mortes"
     },
     {
         id: 13,
         attachment: "images/homemade/fait-maison-legumes-de-saison.jpg",
-        alt:""
+        alt:"o phil des saveurs cuisine avec des legumes frais de saison tous ses plats"
     },
     {
         id: 11,
         attachment: "images/homemade/fait-maison-oignon-frais.jpg",
-        alt:""
+        alt:"o phil des saveurs au marche d aigues mortes propose des plats prepares faits maison"
     },
     {
         id: 10,
         attachment: "images/homemade/fait-maison-o-phil-des-saveurs.jpg",
-        alt:""
+        alt:"o phil des saveurs propose des plats cuisines faits maison au marche d aigues mortes"
     }
 ]
